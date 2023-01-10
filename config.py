3353e46baef3a12428f24ff8fa112fe849a03120
seanpowwer <106429668+seanpowwer@users.noreply.github.com>
2023-01-10 14:53:26 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-USER_AGENT = "hello"
+USER_AGENT = "Mozilla/5.0 (iPad; CPU OS 10_15_5 (Ergänzendes Update) like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/605.1.15"
